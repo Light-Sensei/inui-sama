0e3f61a179ee0f618936f3cc9b89db81e2f61592
Light-Sensei <99256366+Light-Sensei@users.noreply.github.com>
2022-03-13 13:27:59 +0530

    Update __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -368,7 +368,7 @@ def emiko_about_callback(update, context):
             "\n• I have a note keeping system, blacklists, and even predetermined replies on certain keywords."
             "\n• I check for admins' permissions before executing any command and more stuffs"
             "\n\n_Emiko's licensed under the GNU General Public License v3.0_"
-            "\n\n Click on button bellow to get basic help for EmikoRobot.",
+            "\n\n Click on button bellow to get basic help for Inui Robot.",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
@@ -463,7 +463,11 @@ def emiko_about_callback(update, context):
                  [
                     InlineKeyboardButton(text="Light Yagami (夜神月)", url="https://github.com/Light-Sensei"),
                     InlineKeyboardButton(text="Itachi", url="https://github.com/Yoriicci-Tsugikuni"),
-                   ]
+                 ]
+                ]
+                    InlineKeyboardButton(text="Go Back", callback_data="emiko_"),
+                 
+                 ]
                 ]
             ),
         )
